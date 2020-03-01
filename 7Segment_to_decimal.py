4491f5ba590e88d3aa63e524e2f6591980600d6a
Ibrahim-Abu-Rmaileh <ibrahim.abu.rmaileh@gmail.com>
2020-03-01 12:38:24 +0200

    submission 2

--- 7Segment_to_decimal.py
+++ 7Segment_to_decimal.py
@@ -3,6 +3,7 @@
 
 NUMBERS_DECODING={ 63: 0, 6: 1, 91: 2, 79: 3, 102: 4, 109: 5, 125: 6, 7: 7, 127: 8, 111: 9, 0: ' ' }
 
+SEGMENTS_GUIDE={'_':1,'|':2,'-':3}
 
 def converting_from_7segment_to_decimal(filename):
 
@@ -22,7 +23,8 @@ def converting_from_7segment_to_decimal(filename):
             one_line_stored_in_array=[]
             one_line_stored_in_array.append(line)
             print(one_line_stored_in_array)
-
+            # for segment in one_line_stored_in_array:
+            #    print (SEGMENTS_GUIDE["segment"])
 
 
 converting_from_7segment_to_decimal("input_Q1a.txt")
